32045844576c5effcf627b9aa86fcd466e86f35a
Mrunalee Shinde <mrunalss98@gmail.com>
2025-02-09 18:16:52 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/Movies.js';
+import Movies from './components/Movies.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies></Movies>
+    </div>
+  );
+}
+
+export default App;
+
+
+//API Key: 243722cd91d03e9cadd3879ce8330a86
